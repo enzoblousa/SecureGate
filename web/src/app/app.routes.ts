beec79c75b3b2185c8720d4a4f2db48bd18d3f2b
Enzo Spindola <enzoblousa@gmail.com>
2026-07-15 10:30:33 -0300

    feat: add authGuard and wire up register/login/home routes

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './auth/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./auth/register/register.component').then((m) => m.RegisterComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./auth/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'home',
+    canActivate: [authGuard],
+    loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent),
+  },
+  { path: '**', redirectTo: 'login' },
+];
